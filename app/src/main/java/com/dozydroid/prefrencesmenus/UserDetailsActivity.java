60b3dde4f86750cd624cbaf58e6b783ab3ed21d3
Muhammad Waqas <waqasazamkml@gmail.com>
2017-06-11 00:02:48 +0500

    Added menu, sub menu, context menu using XML, Java and their functionality.

--- app/src/main/java/com/dozydroid/prefrencesmenus/UserDetailsActivity.java
+++ app/src/main/java/com/dozydroid/prefrencesmenus/UserDetailsActivity.java
@@ -2,10 +2,14 @@ package com.dozydroid.prefrencesmenus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import java.util.HashMap;
@@ -18,6 +22,8 @@ public class UserDetailsActivity extends AppCompatActivity {
 
     OurPreferences ourPreferences;
     HashMap<String, String> userDetails = new HashMap<>();
+    private String userName;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +32,8 @@ public class UserDetailsActivity extends AppCompatActivity {
 
         ourPreferences = new OurPreferences(UserDetailsActivity.this);
         userDetails = ourPreferences.getUserDetails();
-        String userName = userDetails.get(OurPreferences.KEY_USERNAME);
-        final String password = userDetails.get(OurPreferences.KEY_PASSWORD);
+        userName = userDetails.get(OurPreferences.KEY_USERNAME);
+        password = userDetails.get(OurPreferences.KEY_PASSWORD);
 
         tvUserName = (TextView) findViewById(R.id.tvUsername);
         tvPassword = (TextView) findViewById(R.id.tvPassword);
@@ -54,5 +60,40 @@ public class UserDetailsActivity extends AppCompatActivity {
                     tvPassword.setText("********");
             }
         });
+
+        registerForContextMenu(tvPassword);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemID = item.getItemId();
+        if(itemID == R.id.action_settings){
+
+        }else if(itemID == R.id.action_username){
+            Toast.makeText(this, "Username : "+ userName, Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        menu.add(0, 1, 1, "Show/Hide Password");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if(item.getItemId() == 1){
+            if(tvPassword.getText().toString().equals("********")){
+                tvPassword.setText(password);
+            }else
+                tvPassword.setText("********");
+        }
+        return true;
     }
 }
